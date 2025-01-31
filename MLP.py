f2cb06ac978142e022f78e87a3b7bd356f3b2ddf
Jack <jackj12358@gmail.com>
2025-01-30 22:02:36 -0500

    transforming and plotting images

--- MLP.py
+++ MLP.py
@@ -27,13 +27,52 @@ torch.cuda.manual_seed(SEED)
 torch.backends.cudnn.deterministic = True
 
 ROOT = '.data'
-
+# Downloading our dataset
 train_data = datasets.MNIST(root=ROOT,
                             train=True,
                             download=True)
+
 
 mean = train_data.data.float().mean() / 255
 std = train_data.data.float().std() / 255
 
 print(f'Calculated mean: {mean}')
 print(f'Calculated std: {std}')
+
+train_transforms = transforms.Compose([
+                            transforms.RandomRotation(5, fill=(0,)),
+                            transforms.RandomCrop(28, padding=2),
+                            transforms.ToTensor(),
+                            transforms.Normalize(mean=[mean], std=[std])
+                                      ])
+
+test_transforms = transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize(mean=[mean], std=[std])
+                                     ])
+
+train_data = datasets.MNIST(root=ROOT,
+                            train=True,
+                            download=True,
+                            transform=train_transforms)
+
+test_data = datasets.MNIST(root=ROOT,
+                           train=False,
+                           download=True,
+                           transform=test_transforms)
+
+print(f'Number of training examples: {len(train_data)}')
+print(f'Number of testing examples: {len(test_data)}')
+
+def plot_images(images):
+
+    n_images = len(images)
+
+    rows = int(np.sqrt(n_images))
+    cols = int(np.sqrt(n_images))
+
+    fig = plt.figure()
+    for i in range(rows*cols):
+        ax = fig.add_subplot(rows, cols, i+1)
+        ax.imshow(images[i].view(28, 28).cpu().numpy(), cmap='bone')
+        ax.axis('off')
