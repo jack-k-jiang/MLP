f2a61d10b1d9ae7945783121345169b8d5512376
Jack <jackj12358@gmail.com>
2025-01-30 22:46:04 -0500

    made main default branch

--- MLP.py
+++ MLP.py
@@ -76,3 +76,95 @@ def plot_images(images):
         ax = fig.add_subplot(rows, cols, i+1)
         ax.imshow(images[i].view(28, 28).cpu().numpy(), cmap='bone')
         ax.axis('off')
+
+N_IMAGES = 25
+
+images = [image for image, label in [train_data[i] for i in range(N_IMAGES)]]
+
+plot_images(images)
+
+VALID_RATIO = 0.9
+
+n_train_examples = int(len(train_data) * VALID_RATIO)
+n_valid_examples = len(train_data) - n_train_examples
+
+train_data, valid_data = data.random_split(train_data,
+                                           [n_train_examples, n_valid_examples])
+
+print(f'Number of training examples: {len(train_data)}')
+print(f'Number of validation examples: {len(valid_data)}')
+print(f'Number of testing examples: {len(test_data)}')
+
+
+N_IMAGES = 25
+
+images = [image for image, label in [valid_data[i] for i in range(N_IMAGES)]]
+
+plot_images(images)
+
+valid_data = copy.deepcopy(valid_data)
+valid_data.dataset.transform = test_transforms
+
+N_IMAGES = 25
+
+images = [image for image, label in [valid_data[i] for i in range(N_IMAGES)]]
+
+plot_images(images)
+
+BATCH_SIZE = 64
+
+train_iterator = data.DataLoader(train_data,
+                                 shuffle=True,
+                                 batch_size=BATCH_SIZE)
+
+valid_iterator = data.DataLoader(valid_data,
+                                 batch_size=BATCH_SIZE)
+
+test_iterator = data.DataLoader(test_data,
+                                batch_size=BATCH_SIZE)
+
+class MLP(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+
+        self.input_fc = nn.Linear(input_dim, 250)
+        self.hidden_fc = nn.Linear(250, 100)
+        self.output_fc = nn.Linear(100, output_dim)
+
+    def forward(self, x):
+
+        # x = [batch size, height, width]
+
+        batch_size = x.shape[0]
+
+        x = x.view(batch_size, -1)
+
+        # x = [batch size, height * width]
+
+        h_1 = F.relu(self.input_fc(x))
+
+        # h_1 = [batch size, 250]
+
+        h_2 = F.relu(self.hidden_fc(h_1))
+
+        # h_2 = [batch size, 100]
+
+        y_pred = self.output_fc(h_2)
+
+        # y_pred = [batch size, output dim]
+
+        return y_pred, h_2
+
+INPUT_DIM = 28 * 28
+OUTPUT_DIM = 10
+
+model = MLP(INPUT_DIM, OUTPUT_DIM)
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+print(f'The model has {count_parameters(model):,} trainable parameters')
+
+optimizer = optim.Adam(model.parameters())
+
+criterion = nn.CrossEntropyLoss()
